b75b00e05dc375ce52554ae4b8e208d3bfb64da9
saurabhgdeshmukh <128806572+saurabhgdeshmukh@users.noreply.github.com>
2023-12-10 13:37:03 +0530

    Update Navbar.tsx
    Signed-off-by: saurabhgdeshmukh <128806572+saurabhgdeshmukh@users.noreply.github.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import React,{useState} from 'react'
 import '../index.css'
-import 'tailwindcss/tailwind.css';
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => {
@@ -20,6 +19,19 @@ const Navbar = () => {
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
+  </div>
+    <div className="items-center justify-between w-full md:flex md:w-auto md:order-1 hidden " id="navbar-cta"  >
+    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <li>
+        <a href="#" className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">Home</a>
+      </li>
+      <li>
+        <a href="#benefits" className=" block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Benefits</a>
+      </li>
+      <li>
+        <a href="#main" className=" block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Refactor</a>
+      </li>
+    </ul>
   </div>
   {isMenuOpen &&(
   <div className="items-center justify-between w-full md:flex md:w-auto md:order-1 md:hidden " id="navbar-cta"  >
