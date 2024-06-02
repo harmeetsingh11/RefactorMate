38877420f66d0e99d0e0c0a9feebb551eaa28d8e
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-06-02 21:31:56 +0530

    bug-fix: text in benefits section appears white

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -15,16 +15,16 @@ const AboutUs = () => {
                     <div className='w-11/12 bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg hover:bg-blue-300 dark:hover:bg-blue-600 p-6' onMouseOver={() => setCodeHovered(true)} onMouseLeave={() => setCodeHovered(false)}>
                         <div className='flex items-center gap-4'>
                             <img src={codeHovered ? codehover : code} alt="" className='ml-4 mt-4 w-14 h-14' />
-                            <div className='text-xl font-bold text-white'>Improved Readability and Maintainability</div>
+                            <div className='text-gray-900 dark:text-white text-2xl font-bold mb-2'>Improved Readability and Maintainability</div>
                         </div>
-                        <p className='font-semibold text-white text-md mt-4 mx-4'>Refactoring code is essential for enhancing readability and maintainability. Clear and well-organized code makes it easier for developers to understand the logic, reducing the chances of errors and facilitating collaboration. By refactoring, you invest in code that is not only functional but also sustainable over time, making future modifications and updates more efficient.</p>
+                        <p className='text-lg font-normal text-gray-700 dark:text-gray-400 mb-4 mt-2 text-justify'>Refactoring code is essential for enhancing readability and maintainability. Clear and well-organized code makes it easier for developers to understand the logic, reducing the chances of errors and facilitating collaboration. By refactoring, you invest in code that is not only functional but also sustainable over time, making future modifications and updates more efficient.</p>
                     </div>
                     <div className='about-us-product w-11/12 bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg hover:bg-purple-300 dark:hover:bg-purple-600 p-6' onMouseOver={() => setPerformanceHovered(true)} onMouseLeave={() => setPerformanceHovered(false)}>
                         <div className='flex items-center gap-4'>
                             <img src={performanceHovered ? performancehover : performance} alt="" className='ml-4 mt-4 w-14 h-14' />
-                            <div className='text-xl font-bold text-white'>Enhanced Performance and Efficiency</div>
+                            <div className='text-gray-900 dark:text-white text-xl font-bold mb-2'>Enhanced Performance and Efficiency</div>
                         </div>
-                        <p className='font-semibold text-white text-md mt-4 mx-4'>Code refactoring plays a crucial role in improving the performance and efficiency of your application. Through optimizations and restructuring, you can eliminate bottlenecks, reduce redundancy, and enhance overall system performance. Refactored code tends to be more streamlined and optimized, leading to a more responsive and scalable software solution.</p>
+                        <p className='text-lg font-normal text-gray-700 dark:text-gray-400 mb-4 mt-2 text-justify'>Code refactoring plays a crucial role in improving the performance and efficiency of your application. Through optimizations and restructuring, you can eliminate bottlenecks, reduce redundancy, and enhance overall system performance. Refactored code tends to be more streamlined and optimized, leading to a more responsive and scalable software solution.</p>
                     </div>
                 </div>
             </div>
